59ccb763bbeae5107bd9acca65555a72ee8c0551
FroznMine <froznmine@pesacraft.de>
2015-07-24 16:40:17 -0700

    Add new methods to CannonFighter
    Added a method to get the CannonFighters name.
    
    Removed getAmmo() and getMaxAmmo(), as it isn’t used.
    
    Added getPlayer() to get the player represented by the CannonFighter

--- src/main/java/de/pesacraft/cannonfight/data/players/CannonFighter.java
+++ src/main/java/de/pesacraft/cannonfight/data/players/CannonFighter.java
@@ -5,9 +5,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,8 +18,11 @@ import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import de.pesacraft.cannonfight.CannonFight;
+import de.pesacraft.cannonfight.game.cannons.Cannon;
+import de.pesacraft.cannonfight.game.cannons.Cooldown;
 import de.pesacraft.cannonfight.util.Database;
 import de.pesacraft.lobbysystem.user.User;
 import de.pesacraft.lobbysystem.user.Users;
@@ -27,6 +32,9 @@ public class CannonFighter {
 	private final int id;
 	private int xp;
 	private int level;
+	
+	private List<Cannon> cannons;
+	
 	
 	private CannonFighter(Player p) {
 		ResultSet result = Database.execute("SELECT * FROM " + Database.getTablePrefix() + "players WHERE uuid LIKE %" + p.getUniqueId() + "% LIMIT 1");
@@ -45,7 +53,7 @@ public class CannonFighter {
 		this.id = id;
 		this.user = Users.getByUUID(p.getUniqueId());
 		
-		online.put(p.getName(), value);
+		this.cannons = new ArrayList<Cannon>();
 	}
 
 	public void giveCoins(int amount) {
@@ -63,24 +71,6 @@ public class CannonFighter {
 		cannonLevels.put(cannonName, newLevel);
 		
 		return true;
-	}
-	
-	/**
-	 * Get the players current available ammo
-	 * 
-	 * @return the players ammo
-	 */
-	public long getAmmo() {
-		return this.ammo;
-	}
-	
-	/**
-	 * Get the maximum ammo the player can have
-	 * 
-	 * @return the players maximum ammo
-	 */
-	public long getMaxAmmo() {
-		return this.maxAmmo;
 	}
 	
 	public int getID() {
@@ -107,6 +97,10 @@ public class CannonFighter {
 		user.sendMessage(msg);
 	}
 	
+	public String getName() {
+		return user.getPlayer().getName();
+	}
+	
 	private static Map<String, CannonFighter> online = new HashMap<String, CannonFighter>();
 	
 	public static CannonFighter get(Player p) {
@@ -121,5 +115,25 @@ public class CannonFighter {
 	
 	public static CannonFighter remove(Player p) {
 		return online.remove(p.getName());
+	}
+
+	public Player getPlayer() {
+		return user.getPlayer();
+	}
+
+	public boolean use(ItemStack item) {
+		// find the used cannon
+		for (Cannon c : cannons) {
+			if (c.getItemStack().equals(item)) {
+				// that is the used cannon
+				try {
+					return c.fire(this);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		
+		return false;
 	}
 }
