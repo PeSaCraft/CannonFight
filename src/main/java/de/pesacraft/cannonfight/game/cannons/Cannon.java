826e216c441e49c9ef3585e7af364227f32b5658
FroznMine <froznmine@pesacraft.de>
2015-07-24 16:35:28 -0700

    Cannon now extends Cooldown
    As a cannon has to cool down it extends Cooldown.
    
    There are also some different abstract methods:
    
    - fire now returns a boolean, true if shooting was successful, false
    otherwise.
    - getItem returns the ItemStack representing the Cannon, previously
    getItemStack
    - hasAmmo returns true if the cannon has ammo left, which means it can
    be used.

--- src/main/java/de/pesacraft/cannonfight/game/cannons/Cannon.java
+++ src/main/java/de/pesacraft/cannonfight/game/cannons/Cannon.java
@@ -4,16 +4,19 @@ import org.bukkit.inventory.ItemStack;
 
 import de.pesacraft.cannonfight.data.players.CannonFighter;
 
-public abstract class Cannon {
+public abstract class Cannon extends Cooldown {
 	
-	public abstract ItemStack getItemStack();
+	public Cannon(int time) {
+		super(time);
+	}
+
+	public abstract ItemStack getItem();
 	
 	public abstract String getName();
 	
 	public abstract int getMaxAmmo();
 	
-	public abstract int getCooldownTime();
+	public abstract boolean fire(CannonFighter player) throws Exception;
 	
-	public abstract void fire(CannonFighter player) throws Exception;
-	
+	public abstract boolean hasAmmo();
 }
