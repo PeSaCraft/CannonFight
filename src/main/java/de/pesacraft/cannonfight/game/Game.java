c54bf2547eb173a0752c0cb8f918585b1615bac6
FroznMine <froznmine@pesacraft.de>
2015-08-01 20:06:00 -0700

    Added new features to Game
    Added CannonFighterJoin listener. The listener will output a message to
    the players in game.
    
    The new GameOver listener will now notify the participants and all
    given winners will be rewarded.
    
    Added messages.

--- src/main/java/de/pesacraft/cannonfight/game/Game.java
+++ src/main/java/de/pesacraft/cannonfight/game/Game.java
@@ -1,6 +1,8 @@
 package de.pesacraft.cannonfight.game;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -8,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -15,7 +18,9 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import de.pesacraft.cannonfight.CannonFight;
+import de.pesacraft.cannonfight.Language;
 import de.pesacraft.cannonfight.api.game.CannonFighterDeathEvent;
+import de.pesacraft.cannonfight.api.game.CannonFighterJoinGameEvent;
 import de.pesacraft.cannonfight.api.game.GameOverEvent;
 import de.pesacraft.cannonfight.data.players.CannonFighter;
 
@@ -25,6 +30,7 @@ public class Game implements Listener {
 	private GameState state;
 	private int players;
 	private int time;
+	
 	private BukkitTask countdownTask;
 	
 	protected Game(Arena arena) {
@@ -41,10 +47,19 @@ public class Game implements Listener {
 		if (players == getMaxPlayers())
 			return false;
 	
+		CannonFighterJoinGameEvent event = new CannonFighterJoinGameEvent(this, p);
+		Bukkit.getServer().getPluginManager().callEvent(event);
+		
+		if (event.isCancelled())
+			return false;
+		
 		arena.teleport(p, this);
+		
+		p.sendMessage(Language.get("info.player-join"));
 		
 		for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
 			entry.getKey().show(p);
+			entry.getKey().sendMessage(Language.get("info.player-join-others"));
 			switch (entry.getValue()) {
 			case PLAYER:
 				p.show(entry.getKey());
@@ -57,9 +72,10 @@ public class Game implements Listener {
 		
 		participants.put(p, Role.PLAYER);
 		players++;
-		if (players == arena.getRequiredPlayers()) {
+		
+		if (players == arena.getRequiredPlayers())
 			start();
-		}
+		
 		return true;
 	}
 	
@@ -110,12 +126,12 @@ public class Game implements Listener {
 				state = GameState.INGAME;
 				nextCountdown();
 				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(ChatColor.GREEN + "LOS!");
+					c.sendMessage(Language.get("info.game-start")); // ChatColor.GREEN + "LOS!"
 				}
 				break;	
 			}
 			for (CannonFighter c : participants.keySet()) {
-				c.sendMessage(ChatColor.AQUA + "Noch " + time + " Sekunden bis zum Start!");
+				c.sendMessage(Language.get("info.game-start-countdown")); // ChatColor.AQUA + "Noch " + time + " Sekunden bis zum Start!"
 			}
 			
 			break;
@@ -123,20 +139,26 @@ public class Game implements Listener {
 			if (time == 0) {
 				state = GameState.GAMEOVER;
 				nextCountdown();
-				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(ChatColor.RED + "Spiel vorbei!");
-				}
+				
+				List<CannonFighter> winner = new ArrayList<CannonFighter>();
+				
+				for (Entry<CannonFighter, Role> entry : participants.entrySet())
+					if (entry.getValue() == Role.PLAYER)
+						winner.add(entry.getKey());
+				
+				Bukkit.getServer().getPluginManager().callEvent(new GameOverEvent(this, winner));
+				
 				break;	
 			}
 			// jede Minute uebrige Zeit, wenn weniger als 1 min alle 15 sek und die letzten 15 sek.
 			if (time % 60 == 0) {
 				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(ChatColor.AQUA + "Noch " + time % 60 + " Minuten bis zum Ende!");
+					c.sendMessage(Language.get("info.game-remaining-min")); // ChatColor.AQUA + "Noch " + time % 60 + " Minuten bis zum Ende!"
 				}
 			}
 			if ((time < 60 && time % 15 == 0) || (time < 15)) {
 				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(ChatColor.AQUA + "Noch " + time + " Sekunden bis zum Ende!");
+					c.sendMessage(Language.get("info.game-remaining-sec")); // ChatColor.AQUA + "Noch " + time + " Sekunden bis zum Ende!"
 				}	
 			}
 			
@@ -187,7 +209,11 @@ public class Game implements Listener {
 					state = GameState.GAMEOVER;
 					nextCountdown();
 					
-					Bukkit.getServer().getPluginManager().callEvent(new GameOverEvent(this, entry.getKey()));
+					List<CannonFighter> winner = new ArrayList<CannonFighter>();
+					
+					winner.add(entry.getKey());
+					
+					Bukkit.getServer().getPluginManager().callEvent(new GameOverEvent(this, winner));
 					
 					break;
 				}
@@ -234,24 +260,48 @@ public class Game implements Listener {
 	}
 	
 	@EventHandler
+	public void onGameOver(GameOverEvent event) {
+		for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
+			entry.getKey().sendMessage(Language.get("info.game-over")); // ChatColor.RED + "Spiel vorbei!"
+		}
+		
+		for (CannonFighter c : event.getWinner()) {
+			c.sendMessage(Language.get("info.game-won"));
+			c.giveCoins(CannonFight.PLUGIN.getConfig().getInt("game.reward"));
+		}
+	}
+	
+	@EventHandler
 	public void onCannonFighterDeath(CannonFighterDeathEvent event) {
 		if (event.getGame() == this) {
 			String msg;
 			
-			if (event.getKiller() != null)
+			if (event.getKiller() != null) {
 				// von spieler getoetet
-				msg = event.getVictim().getName() + " wurde von " + event.getKiller().getName() + " getötet.";
-			else
+				msg = Language.get("info.player-died-killed-by-other"); // event.getVictim().getName() + " wurde von " + event.getKiller().getName() + " getötet."
+				event.getKiller().sendMessage(Language.get("info.player-killed"));
+				event.getVictim().sendMessage(Language.get("info.player-got-killed"));
+			}
+			else {
 				// anderer todes grund
-				msg = event.getVictim().getName() + " ist gestorben.";	
+				msg = Language.get("info.player-died"); // event.getVictim().getName() + " ist gestorben.";	
+				event.getVictim().sendMessage(Language.get("info.player-died-self"));
+			}
 			
 			for (CannonFighter c : participants.keySet()) {
 				c.sendMessage(msg);
-				c.sendMessage(players + " Spieler übrig!");
+				c.sendMessage(Language.get("info.remaining-players")); // players + " Spieler übrig!"
 			}
 		}
 	}
-
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onCannonFighterJoin(CannonFighterJoinGameEvent event) {
+		if (event.getGame() == this && !event.isCancelled()) {
+			for (CannonFighter c : participants.keySet()) {
+				c.sendMessage(Language.get("info.player-joined")); // players + " Spieler übrig!"
+			}
+		}
+	}
 	public String getPosition() {
 		return "CannonFight" + (state == GameState.INGAME ? " in der Arena " + arena.getPosition() : "");
 	}
