a428359b2093d050cfd92b83e0923b937c68b4a2
FroznMine <pesafrre@arcor.de>
2015-08-15 00:16:24 +0200

    CannonConstructor now additionally takes the CannonFighter.
    That is necessary as the Cannons will store the player who owns them.
    Without that argument it wouldn't be possible to create some Cannons.

--- src/main/java/de/pesacraft/cannonfight/game/cannons/CannonConstructor.java
+++ src/main/java/de/pesacraft/cannonfight/game/cannons/CannonConstructor.java
@@ -2,8 +2,10 @@ package de.pesacraft.cannonfight.game.cannons;
 
 import java.util.Map;
 
+import de.pesacraft.cannonfight.data.players.CannonFighter;
+
 public interface CannonConstructor {
 	public boolean canConstruct(String name);
 	
-	public Cannon construct(Map<String, Object> map);
+	public Cannon construct(CannonFighter fighter, Map<String, Object> map);
 }
