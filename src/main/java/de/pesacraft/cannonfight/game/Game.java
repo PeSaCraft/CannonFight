b041e4e201b7b8700981a85ebfc3bd1a015ddc5f
FroznMine <pesafrre@arcor.de>
2015-08-25 01:34:50 +0200

    Using new participant handling
    This will store players and spectators in two separate lists.
    
    Also the cage handling is now completed. It is done by the ActivePlayer
    object representing the player ingame.
    
    Also changed from MC-version 1.8_R1 to 1.8_R3

--- src/main/java/de/pesacraft/cannonfight/game/Game.java
+++ src/main/java/de/pesacraft/cannonfight/game/Game.java
@@ -8,10 +8,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import net.minecraft.server.v1_8_R1.BlockPosition;
-import net.minecraft.server.v1_8_R1.ChunkCoordIntPair;
-import net.minecraft.server.v1_8_R1.IBlockData;
-import net.minecraft.server.v1_8_R1.PacketPlayOutMultiBlockChange;
+import net.minecraft.server.v1_8_R3.BlockPosition;
+import net.minecraft.server.v1_8_R3.ChunkCoordIntPair;
+import net.minecraft.server.v1_8_R3.IBlockData;
+import net.minecraft.server.v1_8_R3.PacketPlayOutMultiBlockChange;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -21,8 +21,8 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.craftbukkit.v1_8_R1.CraftChunk;
-import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.CraftChunk;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -44,15 +44,22 @@ import de.pesacraft.cannonfight.api.game.CannonFighterDeathEvent;
 import de.pesacraft.cannonfight.api.game.CannonFighterJoinGameEvent;
 import de.pesacraft.cannonfight.api.game.CannonFighterLeaveEvent;
 import de.pesacraft.cannonfight.api.game.GameOverEvent;
+import de.pesacraft.cannonfight.data.players.ActivePlayer;
 import de.pesacraft.cannonfight.data.players.CannonFighter;
+import de.pesacraft.cannonfight.data.players.Participant;
+import de.pesacraft.cannonfight.data.players.Spectator;
 import de.pesacraft.cannonfight.game.cannons.Cannon;
+import de.pesacraft.cannonfight.util.Cage;
+import de.pesacraft.cannonfight.util.CageForm;
 import de.pesacraft.cannonfight.util.ModifiedBlock;
 
 public class Game implements Listener {
 	private Arena arena;
-	private Map<CannonFighter, Role> participants;
+	
+	private List<ActivePlayer> players;
+	private List<Spectator> spectators;
+	
 	private GameState state;
-	private int players;
 	private int time;
 	
 	private Map<Chunk, Set<ModifiedBlock>> destroyedBlocks;
@@ -62,7 +69,9 @@ public class Game implements Listener {
 	protected Game(Arena arena) {
 		this.arena = arena;
 		this.state = GameState.TELEPORTTOARENA;
-		this.participants = new HashMap<CannonFighter, Role>();
+		
+		this.players = new ArrayList<ActivePlayer>();
+		this.spectators = new ArrayList<Spectator>();
 		
 		this.destroyedBlocks = new HashMap<Chunk, Set<ModifiedBlock>>();
 		Bukkit.getServer().getPluginManager().registerEvents(this, CannonFight.PLUGIN);
@@ -72,7 +81,7 @@ public class Game implements Listener {
 		if (state != GameState.TELEPORTTOARENA)
 			return false;
 
-		if (players == getMaxPlayers())
+		if (players.size() == getMaxPlayers())
 			return false;
 	
 		CannonFighterJoinGameEvent event = new CannonFighterJoinGameEvent(this, p);
@@ -81,32 +90,43 @@ public class Game implements Listener {
 		if (event.isCancelled())
 			return false;
 		
-		players++;
 		
-		arena.teleport(p, this);
+		if (!teleportPlayerToArena(p))
+			return false;
 		
-		for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
-			entry.getKey().show(p);
-			switch (entry.getValue()) {
-			case PLAYER:
-				p.show(entry.getKey());
-				break;
-			case SPECTATOR:
-				p.hide(entry.getKey());
-				break;
-			}
-		}
+		ActivePlayer active = new ActivePlayer(p);
 		
-		participants.put(p, Role.PLAYER);
+		active.createCage();
 		
-		p.sendMessage(Language.get("info.player-join").replaceAll("%player%", players + "").replaceAll("%maxPlayer%", getMaxPlayers() + ""));
+		players.add(active);
 		
-		if (players == arena.getRequiredPlayers())
+		p.sendMessage(Language.get("info.player-join").replaceAll("%player%", players.size() + "").replaceAll("%maxPlayer%", getMaxPlayers() + ""));
+		
+		if (players.size() == arena.getRequiredPlayers())
 			start();
 		
 		return true;
 	}
 	
+	private boolean teleportPlayerToArena(CannonFighter p) {
+		if (!arena.teleport(p, this))
+			return false;
+		
+		// teleport successful
+		// show and hide players
+		for (ActivePlayer active : players) {
+			active.getPlayer().show(p);
+			p.show(active.getPlayer());
+		}
+		
+		for (Spectator spectator : spectators) {
+			spectator.getPlayer().show(p);
+			p.hide(spectator.getPlayer());
+		}
+		
+		return true;
+	}
+
 	public int addPlayers(CannonFighter... players) {
 		int i = 0;
 		for (CannonFighter p : players) {
@@ -118,24 +138,30 @@ public class Game implements Listener {
 	}
 	
 	public boolean addSpectator(CannonFighter p) {
-		if (!arena.teleportSpectator(p, this))
-			// konnte nicht teleportiert werden
+		if (!teleportSpectatorToArena(p))
 			return false;
 		
-		for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
-			p.show(entry.getKey());
-			switch (entry.getValue()) {
-			case PLAYER:
-				entry.getKey().hide(p);
-				break;
-			case SPECTATOR:
-				entry.getKey().show(p);
-				break;
-			}
+		spectators.add(new Spectator(p));
+		
+		return true;
+	}
+	
+	private boolean teleportSpectatorToArena(CannonFighter p) {
+		if (!arena.teleportSpectator(p, this))
+			return false;
+		
+		// teleport successful
+		// show and hide players
+		for (ActivePlayer active : players) {
+			active.getPlayer().hide(p);
+			p.show(active.getPlayer());
 		}
 		
-		participants.put(p, Role.SPECTATOR);
-
+		for (Spectator spectator : spectators) {
+			spectator.getPlayer().show(p);
+			p.show(spectator.getPlayer());
+		}
+		
 		return true;
 	}
 	
@@ -155,10 +181,8 @@ public class Game implements Listener {
 		switch (state) {
 		case TELEPORTTOARENA:
 			if (time == 0) {
-				for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
-					if (entry.getValue() == Role.PLAYER) {
-						preparePlayer(entry.getKey());
-					}
+				for (ActivePlayer active : players) {
+					preparePlayer(active.getPlayer());
 				}
 				state = GameState.START;
 				nextCountdown();
@@ -167,36 +191,30 @@ public class Game implements Listener {
 		case START:
 			if (time == 0) {
 				state = GameState.INGAME;
+				
+				removeCages();
 				nextCountdown();
-				msg = Language.get("info.game-start");
-				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(msg); // ChatColor.GREEN + "LOS!"
-					c.getPlayer().setWalkSpeed(0.2f);
-					c.getPlayer().setFlySpeed(0.2f);
-				}
+				
+				sendMessageToAll(Language.get("info.game-start"));
+				
 				break;	
 			}
-			msg = Language.get("info.game-start-countdown").replaceAll("%time%", time + "");
-			for (CannonFighter c : participants.keySet()) {
-				c.sendMessage(msg); 
-			}
+			
+			sendMessageToAll(Language.get("info.game-start-countdown").replaceAll("%time%", time + ""));
 			
 			break;
 		case INGAME:
-			if (players <= 1) {
+			if (players.size() <= 1) {
 				
 				state = GameState.GAMEOVER;
 				nextCountdown();
 				
 				Set<CannonFighter> winner = new HashSet<CannonFighter>();
 				
-				for (Entry<CannonFighter, Role> entry : participants.entrySet()) {
-					if (entry.getValue() == Role.PLAYER) {
-						// spieler der das spiel gewonnen hat	
-						winner.add(entry.getKey());
-						
-						break;
-					}
+				for (ActivePlayer active : players) {
+					// spieler der das spiel gewonnen hat	
+					winner.add(active.getPlayer());				
+					break;
 				}
 				
 				Bukkit.getServer().getPluginManager().callEvent(new GameOverEvent(this, winner));
@@ -209,28 +227,20 @@ public class Game implements Listener {
 				nextCountdown();
 				
 				Set<CannonFighter> winner = new HashSet<CannonFighter>();
-				
-				for (Entry<CannonFighter, Role> entry : participants.entrySet())
-					if (entry.getValue() == Role.PLAYER)
-						winner.add(entry.getKey());
+			
+				for (ActivePlayer active : players)
+					winner.add(active.getPlayer());
 				
 				Bukkit.getServer().getPluginManager().callEvent(new GameOverEvent(this, winner));
 				
 				break;	
 			}
 			// jede Minute uebrige Zeit, wenn weniger als 1 min alle 15 sek und die letzten 15 sek.
-			if (time % 60 == 0) {
-				msg = Language.get("info.game-remaining-min").replaceAll("%time%", (time / 60) + "");
-				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(msg); // ChatColor.AQUA + "Noch " + time % 60 + " Minuten bis zum Ende!"
-				}
-			}
-			if ((time < 60 && time % 15 == 0) || (time < 15)) {
-				msg = Language.get("info.game-remaining-sec").replaceAll("%time%", time + "");
-				for (CannonFighter c : participants.keySet()) {
-					c.sendMessage(msg); // ChatColor.AQUA + "Noch " + time + " Sekunden bis zum Ende!"
-				}	
-			}
+			if (time % 60 == 0)
+				sendMessageToAll(Language.get("info.game-remaining-min").replaceAll("%time%", (time / 60) + ""));
+
+			if ((time < 60 && time % 15 == 0) || (time < 15))
+				sendMessageToAll(Language.get("info.game-remaining-sec").replaceAll("%time%", time + ""));
 			
 			break;
 		case GAMEOVER:
@@ -238,10 +248,12 @@ public class Game implements Listener {
 				state = GameState.TELEPORTBACK;
 				countdownTask.cancel();
 
-				for (CannonFighter c : participants.keySet()) {
-					leave(c);
-				}
-				
+				for (ActivePlayer c : players)
+					leave(c.getPlayer());
+				for (Spectator c : spectators)
+					leave(c.getPlayer());
+				players.clear();
+				spectators.clear();
 				resetArena();
 			}
 			break;
@@ -251,13 +263,16 @@ public class Game implements Listener {
 		time--;	
 	}
 
+	private void removeCages() {
+		for (ActivePlayer player : players)
+			player.destroyCage();
+	}
+
 	private void preparePlayer(CannonFighter c) {
 		
 		Configuration config = CannonFight.PLUGIN.getConfig();
 		
 		Player p = c.getPlayer();
-		
-		createCage(p);
 		
 		p.setMaxHealth(config.getDouble("game.lives.perLive"));
 		p.setHealth(p.getPlayer().getMaxHealth());
@@ -268,31 +283,6 @@ public class Game implements Listener {
 		for (Cannon cannon : c.getActiveItems()) {
 			inv.addItem(cannon.getItem());
 		}
-	}
-
-	private void createCage(Player p) {
-		Block center = p.getEyeLocation().getBlock();
-
-		Block b = center.getRelative(BlockFace.UP);
-		if (!b.getType().isSolid())
-			b.setType(Material.BARRIER);
-		
-		b = center.getRelative(BlockFace.NORTH);
-		if (!b.getType().isSolid())
-			b.setType(Material.BARRIER);
-		
-		b = center.getRelative(BlockFace.EAST);
-		if (!b.getType().isSolid())
-			b.setType(Material.BARRIER);
-		
-		b = center.getRelative(BlockFace.SOUTH);
-		if (!b.getType().isSolid())
-			b.setType(Material.BARRIER);
-		
-		b = center.getRelative(BlockFace.WEST);
-		if (!b.getType().isSolid())
-			b.setType(Material.BARRIER);
-		
 	}
 
 	private void nextCountdown() {
@@ -315,20 +305,19 @@ public class Game implements Listener {
 	}
 	
 	public boolean removePlayer(CannonFighter c) {
-		if (participants.get(c) != Role.PLAYER)
+		if (!players.contains(new Participant(c)))
 			return false;
 		
-		participants.remove(c);
-		players--;
+		players.remove(new Participant(c));
 		
 		return true;	
 	}
 	
 	public boolean removeSpectator(CannonFighter c) {
-		if (participants.get(c) != Role.SPECTATOR)
+		if (!spectators.contains(new Participant(c)))
 			return false;
 		
-		participants.remove(c);
+		spectators.remove(new Participant(c));
 		
 		return true;
 	}
@@ -342,7 +331,8 @@ public class Game implements Listener {
 		Player p = event.getEntity();
 		CannonFighter victim = CannonFighter.get(p);
 		CannonFighter killer = p.getKiller() != null ? CannonFighter.get(p.getKiller()) : null;
-		if (participants.get(victim) == Role.PLAYER) {
+		
+		if (players.contains(new Participant(victim))) {
 			removePlayer(victim);
 			addSpectator(victim);
 			
@@ -355,9 +345,8 @@ public class Game implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		CannonFighter c = CannonFighter.get(event.getPlayer());
 		
-		if (participants.get(c) == Role.PLAYER) {
+		if (players.contains(new Participant(c)))
 			c.use(event.getItem());
-		}
 	}
 
 	@EventHandler
@@ -365,9 +354,7 @@ public class Game implements Listener {
 		if (event.getGame() != this)
 			return;
 		
-		for (CannonFighter c : participants.keySet()) {
-			c.sendMessage(Language.get("info.game-over")); // ChatColor.RED + "Spiel vorbei!"
-		}
+		sendMessageToAll(Language.get("info.game-over")); // ChatColor.RED + "Spiel vorbei!"
 		
 		int reward = CannonFight.PLUGIN.getConfig().getInt("game.reward");
 		String msg = Language.get("info.game-won").replaceAll("%coins%", reward + "");
@@ -393,11 +380,10 @@ public class Game implements Listener {
 				msg = Language.get("info.player-died"); // event.getVictim().getName() + " ist gestorben.";	
 				event.getVictim().sendMessage(Language.get("info.player-died-self"));
 			}
-			String msg2 = Language.get("info.remaining-players").replaceAll("%player%", players + "");
-			for (CannonFighter c : participants.keySet()) {
-				c.sendMessage(msg);
-				c.sendMessage(msg2); // players + " Spieler übrig!"
-			}
+			String msg2 = Language.get("info.remaining-players").replaceAll("%player%", players.size() + "");
+			
+			sendMessageToAll(msg);
+			sendMessageToAll(msg2);
 		}
 	}
 	
@@ -405,17 +391,25 @@ public class Game implements Listener {
 	public void onCannonFighterLeave(CannonFighterLeaveEvent event) {
 		if (event.getGame() != this)
 			return;
+		
+		if (state == GameState.TELEPORTTOARENA || state == GameState.START) {
+			ActivePlayer p = players.get(players.indexOf(new Participant(event.getFighter())));
+			
+			try {
+				p.destroyCage();
+			}
+			catch (IllegalStateException ex) {
+				// no cage existing: ignore error
+			}
+		}
 		if (state != GameState.INGAME)
 			return;
-		
-		String msg;
 			
-		msg = Language.get("info.player-left"); // event.getFighter().getName() + " hat das Spiel verlassen.";	
-		String msg2 = Language.get("info.remaining-players").replaceAll("%player%", players + "");
-		for (CannonFighter c : participants.keySet()) {
-			c.sendMessage(msg);
-			c.sendMessage(msg2); // players + " Spieler übrig!"
-		}
+		String msg = Language.get("info.player-left"); // event.getFighter().getName() + " hat das Spiel verlassen.";	
+		String msg2 = Language.get("info.remaining-players").replaceAll("%player%", players.size() + "");
+		
+		sendMessageToAll(msg);
+		sendMessageToAll(msg2);
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
@@ -431,9 +425,7 @@ public class Game implements Listener {
 		
 		if (event.getGame() == this && !event.isCancelled()) {
 			String msg = Language.get("info.player-join-others").replaceAll("%player%", event.getPlayer().getName());
-			for (CannonFighter c : participants.keySet()) {
-				c.sendMessage(msg);
-			}
+			sendMessageToAll(msg);
 		}
 	}
 	
@@ -463,14 +455,17 @@ public class Game implements Listener {
 	}
 
 	public boolean leave(CannonFighter c) {
-		if (!participants.containsKey(c))
-			// nicht in diesem Spiel
-			return false;
-		
-		if (participants.remove(c) == Role.PLAYER) {
-			players--;
+		Participant part = new Participant(c);
+		if (players.contains(part)) {
+			// normal player: leave event
+			
 			Bukkit.getServer().getPluginManager().callEvent(new CannonFighterLeaveEvent(this, c));
 		}
+		else if (!spectators.contains(part))
+			// not player and not specator: not in game -> cannot leave
+			return false;
+		
+		// normal player or spectator
 		c.leaveGame();
 		return true;
 	}
@@ -491,13 +486,13 @@ public class Game implements Listener {
 	
 	private void resetArena() {
 		for (Entry<Chunk, Set<ModifiedBlock>> entry : destroyedBlocks.entrySet()) {
-			net.minecraft.server.v1_8_R1.Chunk chunk = ((CraftChunk) entry.getKey()).getHandle();
-			net.minecraft.server.v1_8_R1.World world = chunk.getWorld();
+			net.minecraft.server.v1_8_R3.Chunk chunk = ((CraftChunk) entry.getKey()).getHandle();
+			net.minecraft.server.v1_8_R3.World world = chunk.getWorld();
 			
 			
 			for (ModifiedBlock b : entry.getValue()) {
 				BlockPosition bp = new BlockPosition(b.getXOffset(), b.getYOffset(), b.getZOffset());
-				IBlockData ibd = net.minecraft.server.v1_8_R1.Block.getByCombinedId(b.getMaterial().getId() + (b.getData() << 12));
+				IBlockData ibd = net.minecraft.server.v1_8_R3.Block.getByCombinedId(b.getMaterial().getId() + (b.getData() << 12));
 				chunk.a(bp, ibd); // set block
 			}
 
@@ -520,5 +515,14 @@ public class Game implements Listener {
 		return loc.getBlockX() >= lower.getBlockX() && loc.getBlockX() <= upper.getBlockX()
 				&& loc.getBlockY() >= lower.getBlockY() && loc.getBlockY() <= upper.getBlockY()
 				&& loc.getBlockZ() >= lower.getBlockZ() && loc.getBlockZ() <= upper.getBlockZ();
+	}
+	
+	private void sendMessageToAll(String msg) {
+		for (ActivePlayer p : players) {
+			p.getPlayer().sendMessage(msg);
+		}
+		for (Spectator p : spectators) {
+			p.getPlayer().sendMessage(msg);
+		}
 	}
 }
