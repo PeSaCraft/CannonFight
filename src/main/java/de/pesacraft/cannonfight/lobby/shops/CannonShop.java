a4d737c1957689da64fa3d1a9eb3bcab1959d336
FroznMine <pesafrre@arcor.de>
2015-08-25 01:43:42 +0200

    Create CannonShop
    That shop is used to buy new cannons and get to the upgrade menus of
    already bought cannons.

---
+++ src/main/java/de/pesacraft/cannonfight/lobby/shops/CannonShop.java
@@ -0,0 +1,137 @@
+package de.pesacraft.cannonfight.lobby.shops;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import de.pesacraft.cannonfight.CannonFight;
+import de.pesacraft.cannonfight.Language;
+import de.pesacraft.cannonfight.data.players.CannonFighter;
+import de.pesacraft.cannonfight.game.cannons.CannonConstructor;
+import de.pesacraft.cannonfight.game.cannons.Cannons;
+import de.pesacraft.cannonfight.game.cannons.usable.FireballCannon;
+import de.pesacraft.cannonfight.util.shop.ItemSelectEvent;
+import de.pesacraft.cannonfight.util.shop.Shop;
+import de.pesacraft.cannonfight.util.shop.ShopGroup;
+import de.pesacraft.cannonfight.util.shop.ShopMaker;
+import de.pesacraft.cannonfight.util.shop.ClickHandler;
+
+public class CannonShop {
+	private static final ShopGroup shop;
+		
+	static {
+		shop = new ShopGroup(new ShopMaker() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public Shop createShop(CannonFighter c) {
+				final ItemStack fill = new ItemStack(Material.STAINED_GLASS_PANE, 1, DyeColor.GREEN.getData());
+				
+				Set<String> cannons = Cannons.getCannonSet();
+				int rows = (int) Math.ceil(cannons.size() / 7.0) + 2; // 7 items per row, spacer row above and below
+				
+				final List<ItemStack> items = new ArrayList<ItemStack>();
+				
+				for (String cannon : cannons) {
+					ItemStack item;
+					if (c.hasCannon(cannon))
+						// player has this cannon: upgradeable
+						item = setupUpgradeItem(cannon);
+					else
+						// player doesn't own this cannon: buy
+						item = setupBuyItem(cannon);
+					
+					items.add(item);
+				}
+				
+				Shop s = new Shop("CannonFight Shop", new ClickHandler() {
+					
+					@Override
+					public void onItemSelect(ItemSelectEvent event) {
+						ItemStack item = event.getItem();
+						CannonFighter p = event.getFighter();
+						
+						if (item.isSimilar(fill))
+							return;
+						
+						for (ItemStack i : items) {
+							if (item.isSimilar(i)) {
+								String cannon = i.getItemMeta().getDisplayName();
+								if (p.hasCannon(cannon)) {
+									// owns cannon: open upgrade shop
+									p.getCannon(cannon).openShop();
+									return;
+								}
+								
+								// has to buy cannon
+								CannonConstructor constructor = Cannons.getConstructorByName(cannon);
+								
+								if (p.hasEnoughCoins(constructor.getPrice())) {
+									constructor.buyNew(p);
+									p.takeCoins(constructor.getPrice(), cannon + " gekauft");
+									event.setCloseInventory(true);
+								}
+								else {
+									p.sendMessage(Language.get("error.not-enough-coins"));
+								}
+								return;
+							}
+						}
+					}
+				}, rows);
+				
+				s.fill(fill);
+				
+				int pos = 0; // add position
+				for (ItemStack item : items) {
+					s.set((1 + pos / 7) * 9 + 1 + (pos % 7), item); // 7 Items per row, starting at (1,1)
+				}
+				
+				return s;
+			}
+
+			private ItemStack setupBuyItem(String cannonName) {
+				CannonConstructor constructor = Cannons.getConstructorByName(cannonName);
+				ItemStack i = constructor.getItem();
+				
+				ItemMeta m = i.getItemMeta();
+				
+				List<String> lore = new ArrayList<String>();
+				lore.add(ChatColor.RED + "Diese Kanone besitzt du noch nicht.");
+				lore.add(ChatColor.GREEN + "Du kannst sie für " + constructor.getPrice() + " Coins kaufen.");
+				
+				m.setLore(lore);
+				
+				i.setItemMeta(m);
+				return i;
+			}
+
+			private ItemStack setupUpgradeItem(String cannonName) {
+				CannonConstructor constructor = Cannons.getConstructorByName(cannonName);
+				ItemStack i = constructor.getItem();
+				
+				ItemMeta m = i.getItemMeta();
+				
+				List<String> lore = new ArrayList<String>();
+				lore.add(ChatColor.GREEN + "Diese Kanone besitzt du bereits.");
+				lore.add(ChatColor.GREEN + "Klicke um sie zu verbessern.");
+				
+				m.setLore(lore);
+				
+				i.setItemMeta(m);
+				return i;
+			}
+		});
+	}
+	
+	public static void openShopPage(CannonFighter c) {
+		shop.open(c);
+	}
+}
