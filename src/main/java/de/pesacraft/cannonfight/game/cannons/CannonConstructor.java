35140e16ce3c39cb9951435885d0a025ded8bfaf
FroznMine <pesafrre@arcor.de>
2015-08-25 01:39:25 +0200

    Extended CannonConstructor interface
    There are 3 new methods which will allow better access to relevant
    information:
    
    buyNew takes the CannonFighter who wants to buy a new cannon of that
    type and returns a new Cannon created with initial values.
    
    getPrice returns the price to buy that cannon.
    
    getItem returns the ItemStack representing the cannon.

--- src/main/java/de/pesacraft/cannonfight/game/cannons/CannonConstructor.java
+++ src/main/java/de/pesacraft/cannonfight/game/cannons/CannonConstructor.java
@@ -2,10 +2,18 @@ package de.pesacraft.cannonfight.game.cannons;
 
 import java.util.Map;
 
+import org.bukkit.inventory.ItemStack;
+
 import de.pesacraft.cannonfight.data.players.CannonFighter;
 
 public interface CannonConstructor {
 	public boolean canConstruct(String name);
 	
 	public Cannon construct(CannonFighter fighter, Map<String, Object> map);
+
+	public Cannon buyNew(CannonFighter fighter);
+	
+	public int getPrice();
+	
+	public ItemStack getItem();
 }
