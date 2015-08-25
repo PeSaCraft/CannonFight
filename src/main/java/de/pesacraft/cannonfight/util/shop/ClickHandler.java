3ef3767698e98ebb6291488b0efb8a9e0dee77b8
FroznMine <pesafrre@arcor.de>
2015-08-25 14:44:36 +0200

    ClickHandler now listens to InventoryCloseEvents
    Also onItemSelect now is called onItemInteract

--- src/main/java/de/pesacraft/cannonfight/util/shop/ClickHandler.java
+++ src/main/java/de/pesacraft/cannonfight/util/shop/ClickHandler.java
@@ -1,5 +1,9 @@
 package de.pesacraft.cannonfight.util.shop;
 
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
 public interface ClickHandler {
-	public void onItemSelect(ItemSelectEvent event);
+	public void onItemInteract(ItemInteractEvent event);
+	
+	public void onInventoryClose(InventoryCloseEvent event);
 }
