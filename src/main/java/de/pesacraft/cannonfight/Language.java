b679cb510fd335ed2799770e307028bd7961729a
FroznMine <pesafrre@arcor.de>
2015-09-02 00:30:34 +0200

    Made Language user friendlier
    Now the file is called lang_language.properies inside the pluginsfolder
    in lang.
    
    If a message isn’t found a message that informs the player what is
    missing will be created.

--- src/main/java/de/pesacraft/cannonfight/Language.java
+++ src/main/java/de/pesacraft/cannonfight/Language.java
@@ -11,9 +11,10 @@ import org.bukkit.ChatColor;
 
 public class Language {
 	private static ResourceBundle MESSAGES;
+	private static String brand;
 	
 	public static void loadLanguage(String language) {
-		File folder = CannonFight.PLUGIN.getDataFolder();
+		File folder = new File(CannonFight.PLUGIN.getDataFolder() + "/lang/");
 	
 		URL[] urls;
 		try {
@@ -26,16 +27,27 @@ public class Language {
 		
 		String lang[] = language.split("_");
 		
-		File msg = new File(folder.getAbsolutePath() + "/lang/" + language + ".properties");
+		File msg = new File(folder.getAbsolutePath() + "/lang_" + language + ".properties");
 		if (!msg.exists())
-			CannonFight.PLUGIN.saveResource("lang/" + language+  ".properties", false);
+			CannonFight.PLUGIN.saveResource("lang/lang_" + language + ".properties", false);
 		
 		MESSAGES = ResourceBundle.getBundle("lang", new Locale(lang[0], lang[1]), loader);
 		
-		get("info.language-loaded");
+		get("info.language-loaded", false);
+		
+		brand = get("general.brand", false);
 	}
 	
 	public static String get(String key) {
-		return ChatColor.translateAlternateColorCodes('&', MESSAGES.getString(key));
+		return get(key, true);
+	}
+	
+	public static String get(String key, boolean brand) {
+		try {
+			return (brand ? Language.brand : "") + ChatColor.translateAlternateColorCodes('&', MESSAGES.getString(key));
+		}
+		catch (Exception ex) {
+			return get("error.missing-translation").replaceAll("%key%", key);
+		}
 	}
 }
