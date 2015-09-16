cb1d82eb20ab7b03a8131166afca3cf38bf20712
FroznMine <pesafrre@arcor.de>
2015-09-16 09:32:05 +0200

    Added set/give/take coins command
    Now it should be possible to modify the money a player has from inside
    the game.
    
    Closes #10

--- CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/commands/CoinsCommand.java
+++ CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/commands/CoinsCommand.java
@@ -1,13 +1,16 @@
 package de.pesacraft.cannonfight.util.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import de.pesacraft.cannonfight.util.CannonFightUtil;
 import de.pesacraft.cannonfight.util.Language;
 import de.pesacraft.cannonfight.util.CannonFighter;
+import de.pesacraft.cannonfight.util.money.Money;
 
 public class CoinsCommand implements CommandExecutor {
 	
@@ -51,7 +54,97 @@ public class CoinsCommand implements CommandExecutor {
 			return true;
 		}
 		
-		sender.sendMessage(Language.get("error.wrong-usage").replaceAll("%command%", "/" + label + " [player]"));
+		if (args.length == 3) {
+			OfflinePlayer p;
+			CannonFighter c;
+			int amount;
+			
+			switch (args[1].toLowerCase()) {
+			case "set":
+				if (!sender.hasPermission("cannonfight.command.coins.set")) {
+					sender.sendMessage(Language.get("error.no-permission"));
+					return true;
+				}
+			
+				p = Bukkit.getOfflinePlayer(args[0]);
+				
+				if (p == null) {
+					sender.sendMessage(Language.get("error.player-not-known").replaceAll("%player%", args[0]));
+					return true;
+				}
+				
+				c = CannonFighter.get(p);
+				
+				amount = Integer.parseInt(args[2]);
+				
+				if (c.setCoins(amount)) {
+					sender.sendMessage(Language.get("info.coins-set").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+					
+					if (p.isOnline())
+						c.sendMessage(Language.get("info.coins-set-by-other").replaceAll("%player%", sender.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				else {
+					sender.sendMessage(Language.get("info.coins-set-failed").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				return true;
+			case "give":
+				if (!sender.hasPermission("cannonfight.command.coins.give")) {
+					sender.sendMessage(Language.get("error.no-permission"));
+					return true;
+				}
+			
+				p = Bukkit.getOfflinePlayer(args[0]);
+				
+				if (p == null) {
+					sender.sendMessage(Language.get("error.player-not-known").replaceAll("%player%", args[0]));
+					return true;
+				}
+				
+				c = CannonFighter.get(p);
+				
+				amount = Integer.parseInt(args[2]);
+				
+				if (c.giveCoins(amount, sender.toString() + " gave coins")) {
+					sender.sendMessage(Language.get("info.coins-give").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+					
+					if (p.isOnline())
+						c.sendMessage(Language.get("info.coins-give-by-other").replaceAll("%player%", sender.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				else {
+					sender.sendMessage(Language.get("info.coins-give-failed").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				return true;
+			case "take":
+				if (!sender.hasPermission("cannonfight.command.coins.take")) {
+					sender.sendMessage(Language.get("error.no-permission"));
+					return true;
+				}
+			
+				p = Bukkit.getOfflinePlayer(args[0]);
+				
+				if (p == null) {
+					sender.sendMessage(Language.get("error.player-not-known").replaceAll("%player%", args[0]));
+					return true;
+				}
+				
+				c = CannonFighter.get(p);
+				
+				amount = Integer.parseInt(args[2]);
+				
+				if (c.takeCoins(amount, sender.toString() + " took coins")) {
+					sender.sendMessage(Language.get("info.coins-take").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+					
+					if (p.isOnline())
+						c.sendMessage(Language.get("info.coins-take-by-other").replaceAll("%player%", sender.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				else {
+					sender.sendMessage(Language.get("info.coins-take-failed").replaceAll("%player%", c.getName()).replaceAll("%coins%", c.getCoins() + ""));
+				}
+				return true;
+			};
+		}
+		
+		sender.sendMessage(Language.get("error.wrong-usage").replaceAll("%command%", "/" + label + " [player [(set|give|take) amount]]"));
 		return true;
 	}
 }
