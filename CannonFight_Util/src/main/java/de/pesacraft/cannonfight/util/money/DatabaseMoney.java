530568427f8ac8d511e0a204e405bffd955bc640
FroznMine <pesafrre@arcor.de>
2015-09-16 09:24:56 +0200

    Use UUIDs for money instead of CannonFighter
    The only thing that was done with the CannonFighter was getting his
    UUID, this is too complicated.
    
    Existing methods are marked as deprecated and will be removed in the
    near future.
    
    Also setMoney has been added.

--- CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/money/DatabaseMoney.java
+++ CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/money/DatabaseMoney.java
@@ -2,7 +2,10 @@ package de.pesacraft.cannonfight.util.money;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import java.util.UUID;
+
 import org.bson.Document;
+
 import com.mongodb.client.MongoCollection;
 
 import de.pesacraft.cannonfight.util.CannonFighter;
@@ -18,51 +21,81 @@ public class DatabaseMoney implements Money {
 
 	@Override
 	public int getMoney(CannonFighter c) {
-		Document doc = COLLECTION.find(eq("uuid", c.getPlayer().getUniqueId().toString())).first();
+		return getMoney(c.getPlayer().getUniqueId());
+	}
+
+	@Override
+	public boolean giveMoney(CannonFighter c, int amount, String... reason) {
+		return giveMoney(c.getPlayer().getUniqueId(), amount, reason);
+	}
+
+	@Override
+	public boolean takeMoney(CannonFighter c, int amount, String... reason) {
+		return takeMoney(c.getPlayer().getUniqueId(), amount, reason);
+	}
+
+	@Override
+	public boolean hasEnoughMoney(CannonFighter c, int amount) {
+		return hasEnoughMoney(c.getPlayer().getUniqueId(), amount);
+	}
+
+	@Override
+	public int getMoney(UUID uuid) {
+		Document doc = COLLECTION.find(eq("uuid", uuid.toString())).first();
 		
 		if (doc.containsKey("coins"))
 			// has coins
 			return ((Number) doc.get("coins")).intValue();
 		
 		// no coins -> 0 coins, store that
-		COLLECTION.updateOne(eq("uuid", c.getPlayer().getUniqueId().toString()), new Document("$set", new Document("coins", 0)));
-		return 0;	
+		COLLECTION.updateOne(eq("uuid", uuid.toString()), new Document("$set", new Document("coins", 0)));
+		return 0;
 	}
 
 	@Override
-	public boolean giveMoney(CannonFighter c, int amount, String... reason) {
-		if (amount <= 0)
+	public boolean setMoney(UUID uuid, int amount, String... reason) {
+		if (amount < 0)
 			return false;
 		
-		Document doc = COLLECTION.find(eq("uuid", c.getPlayer().getUniqueId().toString())).first();
-		
-		int newAmount = amount;
-		if (doc.containsKey("coins"))
-			newAmount += ((Number) doc.get("coins")).intValue();
-		
-		COLLECTION.updateOne(eq("uuid", c.getPlayer().getUniqueId().toString()), new Document("$set", new Document("coins", newAmount)));
+		COLLECTION.updateOne(eq("uuid", uuid.toString()), new Document("$set", new Document("coins", amount)));
 		
 		return true;
 	}
 
 	@Override
-	public boolean takeMoney(CannonFighter c, int amount, String... reason) {
+	public boolean giveMoney(UUID uuid, int amount, String... reason) {
 		if (amount <= 0)
 			return false;
 		
-		Document doc = COLLECTION.find(eq("uuid", c.getPlayer().getUniqueId().toString())).first();
+		Document doc = COLLECTION.find(eq("uuid", uuid.toString())).first();
+		
+		int newAmount = amount;
+		if (doc.containsKey("coins"))
+			newAmount += ((Number) doc.get("coins")).intValue();
+		
+		COLLECTION.updateOne(eq("uuid", uuid.toString()), new Document("$set", new Document("coins", newAmount)));
+		
+		return true;
+	}
+
+	@Override
+	public boolean takeMoney(UUID uuid, int amount, String... reason) {
+		if (amount <= 0)
+			return false;
+		
+		Document doc = COLLECTION.find(eq("uuid", uuid.toString())).first();
 		
 		int newAmount = ((Number) doc.get("coins")).intValue() - amount;
 		
 		if (newAmount < 0)
 			return false;
 		
-		COLLECTION.updateOne(eq("uuid", c.getPlayer().getUniqueId().toString()), new Document("$set", new Document("coins", newAmount)));
+		COLLECTION.updateOne(eq("uuid", uuid.toString()), new Document("$set", new Document("coins", newAmount)));
 		return true;
 	}
-
+	
 	@Override
-	public boolean hasEnoughMoney(CannonFighter c, int amount) {
-		return getMoney(c) >= amount;
+	public boolean hasEnoughMoney(UUID uuid, int amount) {
+		return getMoney(uuid) >= amount;
 	}
 }
