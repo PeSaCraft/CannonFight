89a30dbcee7993bae48f84636ac536df41b5d431
FroznMine <pesafrre@arcor.de>
2015-10-03 00:41:03 +0200

    Upgrade changer now supports the value and initialisaton
    Now the interface has a lot new methods. There are ones to de/increase
    the price slow/fast and others for the value.
    
    Also there are methods to get the slow/fast price/value change and a
    method to get the initial value/price which will be used to create new
    upgrades

--- CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/shop/upgrade/UpgradeChanger.java
+++ CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/shop/upgrade/UpgradeChanger.java
@@ -2,11 +2,25 @@ package de.pesacraft.cannonfight.util.shop.upgrade;
 
 public interface UpgradeChanger<T> {
 
-	public void increaseFast(Upgrade<T> upgrade);
+	public int getInitialPrice();
 	
-	public void increaseSlow(Upgrade<T> upgrade);
+	public int getSlowPriceChange();
+	public int getFastPriceChange();
 	
-	public void decreaseSlow(Upgrade<T> upgrade);
+	public void increasePriceFast(Upgrade<T> upgrade);
+	public void increasePriceSlow(Upgrade<T> upgrade);
 	
-	public void decreaseFast(Upgrade<T> upgrade);
+	public void decreasePriceSlow(Upgrade<T> upgrade);
+	public void decreasePriceFast(Upgrade<T> upgrade);
+	
+	public T getInitialValue();
+	
+	public T getSlowValueChange();
+	public T getFastValueChange();
+	
+	public void increaseValueFast(Upgrade<T> upgrade);
+	public void increaseValueSlow(Upgrade<T> upgrade);
+	
+	public void decreaseValueSlow(Upgrade<T> upgrade);
+	public void decreaseValueFast(Upgrade<T> upgrade);
 }
