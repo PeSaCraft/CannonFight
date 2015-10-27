93ab9bb2a133a03cd04dcd36813a38976dc48058
FroznMine <pesafrre@arcor.de>
2015-10-27 20:25:41 +0100

    Cleanup setup
    As the values aren’t used they don’t have to be stored.

--- CannonFight_Hub/src/main/java/de/pesacraft/cannonfight/hub/game/Setup.java
+++ CannonFight_Hub/src/main/java/de/pesacraft/cannonfight/hub/game/Setup.java
@@ -1,12 +1,7 @@
 package de.pesacraft.cannonfight.hub.game;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bson.Document;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 
 import com.mongodb.client.MongoCollection;
 
@@ -17,43 +12,20 @@ public class Setup {
 	private static final MongoCollection<Document> COLLECTION;
 	
 	private String name;
-	
-	private int reqPlayers;
-	
-	private Location specSpawn;
-	
-	private List<Location> spawns;
-	
+
 	static {
 		COLLECTION = Collection.ARENAS();
 	}
 	
 	private Setup(String name) {
 		this.name = name;
-		this.reqPlayers = 0;
-		this.specSpawn = null;
-		this.spawns = new ArrayList<Location>();
-		
+
 		Document doc = new Document("name", name);
 		COLLECTION.insertOne(doc);
 	}
 	
 	public Setup(Document doc) {
 		this.name = doc.getString("name");
-		this.reqPlayers = doc.getInteger("requiredPlayers", 0);
-		
-		World world = Bukkit.getWorld(name);
-		Document docLoc = (Document) doc.get("spectatorSpawn");
-		if (docLoc != null)
-			this.specSpawn = new Location(world, docLoc.getDouble("x"), docLoc.getDouble("y"), docLoc.getDouble("z"), docLoc.getDouble("yaw").floatValue(), docLoc.getDouble("pitch").floatValue());
-		
-		List<Document> spawnList = (List<Document>) doc.get("spawns");
-		
-		this.spawns = new ArrayList<Location>();
-		
-		if (spawnList != null)
-			for (Document spawn : spawnList)
-				spawns.add(new Location(world, spawn.getDouble("x"), spawn.getDouble("y"), spawn.getDouble("z"), spawn.getDouble("yaw").floatValue(), spawn.getDouble("pitch").floatValue()));
 	}
 
 	public String getName() {
@@ -64,9 +36,7 @@ public class Setup {
 		if (amount <= 1)
 			return false;
 		
-		this.reqPlayers = amount;
-		
-		COLLECTION.updateOne(eq("name", name), new Document("$set", new Document("requiredPlayers", reqPlayers)));
+		COLLECTION.updateOne(eq("name", name), new Document("$set", new Document("requiredPlayers", amount)));
 		
 		return true;
 	}
@@ -75,8 +45,6 @@ public class Setup {
 		if (!loc.getWorld().getName().equals(name))
 			return false;
 		
-		this.specSpawn = loc;
-
 		Document doc = new Document("x", loc.getX());
 		doc = doc.append("y", loc.getY());
 		doc = doc.append("z", loc.getZ());
@@ -92,8 +60,6 @@ public class Setup {
 	public boolean addSpawn(Location loc) {
 		if (!loc.getWorld().getName().equals(name))
 			return false;
-		
-		this.spawns.add(loc);
 		
 		Document doc = new Document("x", loc.getX());
 		doc = doc.append("y", loc.getY());
