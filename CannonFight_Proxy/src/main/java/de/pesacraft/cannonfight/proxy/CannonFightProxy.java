599b920dd6750c3ccf0b5511c5adb468a930d7a5
FroznMine <pesafrre@arcor.de>
2015-10-29 12:42:54 +0100

    Remove old money system
    As we will only use MongoDB no other possibilities need to be given

--- CannonFight_Proxy/src/main/java/de/pesacraft/cannonfight/proxy/CannonFightProxy.java
+++ CannonFight_Proxy/src/main/java/de/pesacraft/cannonfight/proxy/CannonFightProxy.java
@@ -5,20 +5,15 @@ import java.util.logging.Logger;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import de.pesacraft.cannonfight.util.communication.CommunicationServer;
-import de.pesacraft.cannonfight.util.money.DatabaseMoney;
-import de.pesacraft.cannonfight.util.money.Money;
 
 public class CannonFightProxy extends Plugin implements Listener {
 	public static Logger LOGGER;
 	public static CannonFightProxy PLUGIN;
-	public static Money MONEY;
 	
 	@Override
 	public void onEnable() { 
 		PLUGIN = this;
 		LOGGER = this.getLogger();
-		
-		MONEY = new DatabaseMoney();
 		
 		new CommunicationServer().start();
 	}
--- CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/CannonFightUtil.java
+++ CannonFight_Util/src/main/java/de/pesacraft/cannonfight/util/CannonFightUtil.java
@@ -11,13 +11,9 @@ import java.util.Arrays;
 
 import org.bukkit.plugin.Plugin;
 
-import de.pesacraft.cannonfight.util.money.DatabaseMoney;
-import de.pesacraft.cannonfight.util.money.Money;
-
 public class CannonFightUtil {
 
 	public static Plugin PLUGIN;
-	public static Money MONEY;
 	
 	public static void use(Plugin plugin) {
 		if (PLUGIN != null)
@@ -25,8 +21,6 @@ public class CannonFightUtil {
 		PLUGIN = plugin;
 		
 		MongoDatabase.setup();
-		
-		MONEY = new DatabaseMoney();
 	}
 	
 	public static void copyWorld(File source, File target){
